0d64d46b30c6b608d148b2025fb4bdd2d478a6b4
Matt Filetto <matt.filetto@gmail.com>
2017-01-21 03:25:38 -0800

    bring back super old ril stack. probably wont work

--- ril/telephony/java/com/android/internal/telephony/LgeLteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/LgeLteRIL.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2016 The CyanogenMod Project
+\/*
+ * Copyright (C) 2012 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,16 @@ import static com.android.internal.telephony.RILConstants.*;
 import android.content.Context;
 import android.os.Message;
 import android.os.Parcel;
+import android.os.SystemProperties;
+import android.telephony.SignalStrength;
+import android.text.TextUtils;
+import android.telephony.Rlog;
+
+import java.util.ArrayList;
+
+import com.android.internal.telephony.uicc.IccCardApplicationStatus;
+import com.android.internal.telephony.uicc.IccCardStatus;
+import com.android.internal.telephony.dataconnection.DataCallResponse;
 
 /**
  * Custom Qualcomm RIL for LG G5
@@ -29,13 +39,12 @@ import android.os.Parcel;
  */
 public class LgeLteRIL extends RIL implements CommandsInterface {
 
-    public static final int RIL_UNSOL_AVAILABLE_RAT = 1054;
-    public static final int RIL_UNSOL_LOG_RF_BAND_INFO = 1165;
-    public static final int RIL_UNSOL_LTE_REJECT_CAUSE = 1187;
 
     public LgeLteRIL(Context context, int preferredNetworkType, int cdmaSubscription) {
         super(context, preferredNetworkType, cdmaSubscription, null);
     }
+    boolean RILJ_LOGV = true;
+    boolean RILJ_LOGD = true;
 
     public LgeLteRIL(Context context, int preferredNetworkType,
             int cdmaSubscription, Integer instanceId) {
@@ -44,18 +53,145 @@ public class LgeLteRIL extends RIL implements CommandsInterface {
 
     @Override
     public void
-    setNetworkSelectionModeManual(String operatorNumeric, Message response) {
+    setupDataCall(String radioTechnology, String profile, String apn,
+            String user, String password, String authType, String protocol,
+            Message result) {
+
+        RILRequest rrSPT = RILRequest.obtain(
+                121, null); //121 - RIL_REQUEST_VSS_SET_PDN_TABLE
+        rrSPT.mp.writeInt(1); // pdnId
+        rrSPT.mp.writeInt(apn.length()); // apnLength
+        rrSPT.mp.writeString(apn); // apn
+        rrSPT.mp.writeInt(0); // ipType
+        rrSPT.mp.writeInt(0); // inactivityTime
+        rrSPT.mp.writeInt(1); // enable
+        send(rrSPT);
+
+
+
         RILRequest rr
-                = RILRequest.obtain(RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL,
-                                    response);
+                = RILRequest.obtain(RIL_REQUEST_SETUP_DATA_CALL, result);
 
-        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
-                    + " " + operatorNumeric);
+        rr.mp.writeInt(7);
 
-        rr.mParcel.writeInt(2);
-        rr.mParcel.writeString(operatorNumeric);
-        rr.mParcel.writeString("2"); // NOCHANGE
+        rr.mp.writeString(radioTechnology);
+        rr.mp.writeString(profile);
+        rr.mp.writeString(apn);
+        rr.mp.writeString(user);
+        rr.mp.writeString(password);
+        rr.mp.writeString(authType);
+        rr.mp.writeString(protocol);
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> "
+                + requestToString(rr.mRequest) + " " + radioTechnology + " "
+                + profile + " " + apn + " " + user + " "
+                + password + " " + authType + " " + protocol);
 
         send(rr);
     }
+    */
+
+    @Override
+    protected Object
+    responseSetupDataCall(Parcel p) {
+        DataCallResponse dataCall;
+
+        boolean oldRil = needsOldRilFeature("datacall");
+
+        if (!oldRil)
+           return super.responseSetupDataCall(p);
+
+        p.readString();
+        return super.responseSetupDataCall(p);
+
+    }
+    @Override
+    protected Object
+    responseIccCardStatus(Parcel p) {
+        IccCardApplicationStatus ca;
+
+        IccCardStatus status = new IccCardStatus();
+        status.setCardState(p.readInt());
+        status.setUniversalPinState(p.readInt());
+        status.mGsmUmtsSubscriptionAppIndex = p.readInt();
+        status.mCdmaSubscriptionAppIndex = p.readInt();
+
+        status.mImsSubscriptionAppIndex = p.readInt();
+
+        int numApplications = p.readInt();
+
+        // limit to maximum allowed applications
+        if (numApplications > IccCardStatus.CARD_MAX_APPS) {
+            numApplications = IccCardStatus.CARD_MAX_APPS;
+        }
+        status.mApplications = new IccCardApplicationStatus[numApplications];
+
+
+        for (int i = 0; i < numApplications; i++) {
+            ca = new IccCardApplicationStatus();
+            ca.app_type = ca.AppTypeFromRILInt(p.readInt());
+            ca.app_state = ca.AppStateFromRILInt(p.readInt());
+            ca.perso_substate = ca.PersoSubstateFromRILInt(p.readInt());
+            ca.aid = p.readString();
+            ca.app_label = p.readString();
+            ca.pin1_replaced = p.readInt();
+            ca.pin1 = ca.PinStateFromRILInt(p.readInt());
+            ca.pin2 = ca.PinStateFromRILInt(p.readInt());
+            p.readInt(); //remaining_count_pin1
+            p.readInt(); //remaining_count_puk1
+            p.readInt(); //remaining_count_pin2
+            p.readInt(); //remaining_count_puk2
+            status.mApplications[i] = ca;
+        }
+        int appIndex = -1;
+        if (mPhoneType == RILConstants.CDMA_PHONE) {
+            appIndex = status.mCdmaSubscriptionAppIndex;
+            Rlog.d(RILJ_LOG_TAG, "This is a CDMA PHONE " + appIndex);
+        } else {
+            appIndex = status.mGsmUmtsSubscriptionAppIndex;
+            Rlog.d(RILJ_LOG_TAG, "This is a GSM PHONE " + appIndex);
+        }
+
+        if (numApplications > 0) {
+            IccCardApplicationStatus application = status.mApplications[appIndex];
+            mAid = application.aid;
+            mUSIM = application.app_type
+                      == IccCardApplicationStatus.AppType.APPTYPE_USIM;
+            mSetPreferredNetworkType = mPreferredNetworkType;
+
+            if (TextUtils.isEmpty(mAid))
+               mAid = "";
+            Rlog.d(RILJ_LOG_TAG, "mAid " + mAid);
+        }
+
+        return status;
+    }
+
+    @Override
+    protected Object
+    responseSignalStrength(Parcel p) {
+        int numInts = 12;
+        int response[];
+
+        boolean oldRil = needsOldRilFeature("signalstrength");
+        boolean noLte = false;
+
+        /* TODO: Add SignalStrength class to match RIL_SignalStrength */
+        response = new int[numInts];
+        for (int i = 0 ; i < numInts ; i++) {
+            if ((oldRil || noLte) && i > 6 && i < 12) {
+                response[i] = -1;
+            } else {
+                response[i] = p.readInt();
+            }
+            if (i == 7 && response[i] == 99) {
+                response[i] = -1;
+                noLte = true;
+            }
+        }
+
+        return new SignalStrength(response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7],response[8], response[9], response[10], response[11], true);
+    }
+
 }
+
