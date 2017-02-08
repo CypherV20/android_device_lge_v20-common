0024b881e9240145c788f4bb26e1cef489f475f3
Matt Filetto <matt.filetto@gmail.com>
2017-02-07 16:27:01 -0800

    Revert "shim for imdatadaemon"
    This reverts commit 1b534f8585707bf2eea6183c085984ad877e79df.

--- liblge/lge_ril.cpp
+++ liblge/lge_ril.cpp
@@ -16,8 +16,6 @@
 
 #include <stdlib.h>
 
-int _ZN10CneFactory23CreateCneNetworkRequestE10CnoNetTypeP27ICneNetworkObserverNotifier(CnoNetType, ICneNetworkObserverNotifier) {return 0;}
-
 #ifdef LIBLGE_64BIT
 extern "C" int _ZNK7android6Parcel10readUint64EPm(uint32_t *pArg);
 
